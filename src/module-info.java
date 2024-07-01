d6efae5a01226bbce8d4f092d67bc2ca0d267453
helloworld114514 <1423201097@qq.com>
2024-07-01 10:42:58 +0800

    项目创建

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+open module Match3Game {
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.swing;
+    requires javafx.fxml;
+    requires javafx.swt;
+    requires javafx.media;
+}
